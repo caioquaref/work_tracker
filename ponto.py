4f32c828ddfeb5f542886a228c68ebc8d235be1b
caioquaref <caio.qfmecanica@gmail.com>
2022-01-11 20:14:37 -0300

    simple changes
    changing some stuff to make it better looking.

--- ponto.py
+++ ponto.py
@@ -1,56 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-import pandas as pd
-import tkinter as tk
-from tkinter import messagebox
-
-root= tk.Tk()
-root.geometry('1x1')
-
-def horas_result():
-    '''
-    Análise breve das horas trabalhadas. O argumento 'caminho' diz o nome do arquivo, considerando que ele está salvo na mesma pasta.
-    '''
-    # Importando dados
-    df = pd.read_csv('./pontos_marcados.txt',delimiter=',')
-
-    # Transformando os dados para o tipo correto. De 'string' para 'timedelta'.
-    df['horas_trabalhadas'] = pd.DatetimeIndex(df.saida) - pd.DatetimeIndex(df.entrada)
-    df['data'] = pd.to_datetime(df.data)
-    df['entrada'] = pd.DatetimeIndex(df.entrada).time
-    df['saida'] = pd.DatetimeIndex(df.saida).time
-
-    # Criando coluna de horas excedentes ou faltantes.
-    df['horas_por_dia'] = timedelta(hours=7)
-    df['horas_excedentes_faltantes'] = df.horas_trabalhadas - df.horas_por_dia    
-
-    total = df.horas_excedentes_faltantes.sum()
-    b = timedelta()
-
-    # Definindo o texto na janela popup.
-    if total < b:
-        base = timedelta(days=-1,hours=24)
-        resultado = base - total
-        msg = 'Faltam {} horas.'.format(str(resultado))
-        MsgBox = tk.messagebox.showinfo("Folha", msg)
-        if MsgBox == 'ok':
-            root.destroy()
-        else:
-            root.destroy()
-
-    else:
-        msg = 'Você excedeu {} horas.'.format(str(total))
-        MsgBox = tk.messagebox.showinfo("Folha de pontos", msg)
-        if MsgBox == 'ok':
-            root.destroy()
-        else:
-            root.destroy()
-
-
 # In[2]:
 
 
@@ -96,6 +46,56 @@ def ponto():
         # Criando arquivo.
         with open('pontos_marcados.txt', 'x') as f:
             f.write('data,entrada,saida')
+
+
+# In[1]:
+
+
+import pandas as pd
+import tkinter as tk
+from tkinter import messagebox
+
+root= tk.Tk()
+root.geometry('1x1')
+
+def horas_result():
+    '''
+    Análise breve das horas trabalhadas.
+    '''
+    # Importando dados
+    df = pd.read_csv('./pontos_marcados.txt',delimiter=',')
+
+    # Transformando os dados para o tipo correto. De 'string' para 'timedelta'.
+    df['horas_trabalhadas'] = pd.DatetimeIndex(df.saida) - pd.DatetimeIndex(df.entrada)
+    df['data'] = pd.to_datetime(df.data)
+    df['entrada'] = pd.DatetimeIndex(df.entrada).time
+    df['saida'] = pd.DatetimeIndex(df.saida).time
+
+    # Criando coluna de horas excedentes ou faltantes.
+    df['horas_por_dia'] = timedelta(hours=7)
+    df['horas_excedentes_faltantes'] = df.horas_trabalhadas - df.horas_por_dia    
+
+    total = df.horas_excedentes_faltantes.sum()
+    b = timedelta()
+
+    # Definindo o texto na janela popup.
+    if total < b:
+        base = timedelta(days=-1,hours=24)
+        resultado = base - total
+        msg = 'Faltam {} horas.'.format(str(resultado))
+        MsgBox = tk.messagebox.showinfo("Folha de pontos", msg)
+        if MsgBox == 'ok':
+            root.destroy()
+        else:
+            root.destroy()
+
+    else:
+        msg = 'Você excedeu {} horas.'.format(str(total))
+        MsgBox = tk.messagebox.showinfo("Folha de pontos", msg)
+        if MsgBox == 'ok':
+            root.destroy()
+        else:
+            root.destroy()
 
 
 # In[4]:
